4f6ede447bbb89fba0a5247aae67bd1ba2ae007f
evilfeeh <felipegoncalvesdsantos@gmail.com>
2024-11-19 23:33:21 -0300

    feat: add server.ts

---
+++ src/External/Api/Server.ts
@@ -0,0 +1,21 @@
+import 'reflect-metadata'
+import dotenv from 'dotenv'
+import app from './App'
+
+dotenv.config()
+
+const port = process.env.PORT || 3001
+
+app.listen(port, () => {
+    console.log(
+        `[server]: Server is up and running at http://localhost:${port} 🚀`
+    )
+})
+
+app.get('/', (req, res) => {
+    res.redirect('/public/docs')
+})
+
+app.get('/ping', (req, res) => {
+    res.send('pong')
+})
